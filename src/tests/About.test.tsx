bfeb7467f33eb2ec620a4f14202059edeb26bd15
Sebastiaobnj <junin_sbn@hotmail.com>
2023-08-24 18:26:44 -0300

    construindo o requisito 2

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { About } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+test('Teste se a página contém as informações sobre a Pokédex:', () => {
+  renderWithRouter(<About />);
+  const textAbout = screen.getByRole('heading', { name: 'About Pokédex' });
+
+  expect(textAbout).toBeInTheDocument();
+});
+
+test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.:', () => {
+  renderWithRouter(<About />);
+  const textAbout = screen.getByRole('heading', { name: 'About Pokédex' });
+
+  expect(textAbout).toBeInTheDocument();
+});
--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -2,15 +2,11 @@ import { screen } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
-test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação:', () => {
-  // acessar os elementos na tela
+test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação: Home, About e Favorite Pokémon.', () => {
   renderWithRouter(<App />);
   const linkHome = screen.getByRole('link', { name: 'Home' });
   const linkAbout = screen.getByRole('link', { name: 'About' });
   const linkFavorite = screen.getByRole('link', { name: 'Favorite Pokémon' });
-  // agir interagindo com os elementos da tela (se necessário0
-
-  // aferir
 
   expect(linkHome).toBeInTheDocument();
   expect(linkAbout).toBeInTheDocument();
