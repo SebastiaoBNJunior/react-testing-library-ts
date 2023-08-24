6ef4ba27b74ad732296247b0bc7f57661d688b98
Sebastiaobnj <junin_sbn@hotmail.com>
2023-08-24 20:46:13 -0300

    finalizado o requisito 2

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -2,16 +2,16 @@ import { screen } from '@testing-library/react';
 import { About } from '../pages';
 import renderWithRouter from '../renderWithRouter';
 
-test('Teste se a página contém as informações sobre a Pokédex:', () => {
+test('Teste se a página contém um heading h2 com o texto About Pokédex:', () => {
   renderWithRouter(<About />);
+  // Teste se a página contém um heading h2 com o texto About Pokédex.
   const textAbout = screen.getByRole('heading', { name: 'About Pokédex' });
-
   expect(textAbout).toBeInTheDocument();
-});
-
-test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.:', () => {
-  renderWithRouter(<About />);
-  const textAbout = screen.getByRole('heading', { name: 'About Pokédex' });
-
-  expect(textAbout).toBeInTheDocument();
+  // Teste se a página contém dois parágrafos com texto sobre a Pokédex.
+  const paragraphs = screen.getAllByText(/Pokédex/i);
+  expect(paragraphs).toHaveLength(2);
+  // Teste se a página contém a seguinte imagem de uma Pokédex:
+  const pokedexImage = screen.getByAltText('Pokédex');
+  expect(pokedexImage).toBeInTheDocument();
+  expect(pokedexImage.getAttribute('src')).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
 });
