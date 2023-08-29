51649a9cb1839dc95a2f857e8e2aae7102a1c4df
Sebastiaobnj <junin_sbn@hotmail.com>
2023-08-29 15:18:04 -0300

    finalizado o requisito 4

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,14 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('4. Teste o componente <NotFound.tsx />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found.', () => {
+    renderWithRouter(<NotFound />);
+    const textHeading = screen.getByRole('heading', {
+      name: 'Page requested not found',
+    });
+    expect(textHeading).toBeInTheDocument();
+  });
+});
