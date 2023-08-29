1f99d7062c409f471e559ba53afc99e37fea42c0
Sebastiaobnj <junin_sbn@hotmail.com>
2023-08-29 18:26:38 -0300

    desenvolvendo o requisito 5

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -1,1 +1,13 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { Pokedex } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('5. Teste o componente <Pokedex.tsx />', () => {
+  test('Teste se a página contém um heading h2 com o texto Encountered Pokémon.', () => {
+    renderWithRouter(<Pokedex pokemonList={ [] } favoritePokemonIdsObj={ {} } />);
+    const textHeading = screen.getByRole('heading', {
+      name: 'Encountered Pokémon',
+    });
+    expect(textHeading).toBeInTheDocument();
+  });
+});
