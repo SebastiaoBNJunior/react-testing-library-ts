acd6e1f17e44f2416c4d3474f1004fce51125e38
Sebastiaobnj <junin_sbn@hotmail.com>
2023-08-30 19:46:24 -0300

    corrigindo lint

--- src/tests/Pokemon.test.tsx
+++ src/tests/Pokemon.test.tsx
@@ -1,5 +1,4 @@
 import { screen } from '@testing-library/react';
-import { toBeInTheDocument } from '@testing-library/jest-dom/matchers';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
