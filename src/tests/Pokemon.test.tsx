b64c39ebb59912fe50d19d4d53c1c39476f6da27
Sebastiaobnj <junin_sbn@hotmail.com>
2023-08-29 22:42:42 -0300

    construindo o requisito 6

--- src/tests/Pokemon.test.tsx
+++ src/tests/Pokemon.test.tsx
@@ -1,1 +1,13 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('6. Teste o componente <Pokemon.tsx />', () => {
+  test('Teste se o card do Pokémon indicado na Pokédex contém um link de navegação para exibir detalhes desse Pokémon:', () => {
+    const { user } = renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    expect(moreDetails).toHaveAttribute('to', `/pokemon/${id}`);
+  });
+});
